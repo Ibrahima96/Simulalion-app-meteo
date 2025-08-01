4e7d18762532a1976d94ba8fda6475e4c2339d37
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2025-08-01 16:47:57 +0000

    Il sembl

---
+++ script.js
@@ -0,0 +1,36 @@
+const loader = document.querySelector(".weather-app__loader-container");
+const errorInfo = document.querySelector(".weather-app__error-info");
+
+async function getWeatherData() {
+  loader.classList.add("js-loader-active");
+  let data;
+  try {
+    const response = await fetch("./weatherAPI/weatherData.json");
+    if (!response.ok) {
+      throw new Error("Erreur lors de la récupération des données");
+    }
+    data = await response.json();
+  } catch (error) {
+    errorInfo.textContent = error.message;
+  } finally {
+    loader.classList.remove("js-loader-active");
+  }
+  console.log(data);
+
+  populateUI(data[0]);
+}
+
+getWeatherData();
+
+const tempName = document.querySelector(".weather-app__temp");
+const cityName = document.querySelector(".weather-app__city");
+const countryName = document.querySelector(".weather-app__country");
+const iconeInfo = document.querySelector(".weather-app__info-icon");
+
+function populateUI(data) {
+  cityName.textContent = data.city;
+  countryName.textContent = data.country;
+  tempName.textContent = `${data.temperature}°`;
+  iconeInfo.src = `./ressources/icons/${data.iconID}.svg`;
+  iconeInfo.style.display = "block";
+}
